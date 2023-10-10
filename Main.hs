372f5b422c35d0c6a737798c03f2cb5cc20dcf6d
gijskoppenberg <g.c.koppenberg@students.uu.nl>
2023-10-10 15:31:51 +0200

    added everything from the template, i think...

--- Main.hs
+++ Main.hs
@@ -1,11 +1,19 @@
 module Main where
 
-import Data.Char
-import Data.List
-import Data.Maybe
-import System.IO
-import Distribution.Simple.InstallDirs (PathTemplate)
-import Distribution.Make (unComponentId)
+import Controller
+import Model
+import View
+
+import Graphics.Gloss.Interface.IO.Game
+
+main :: IO ()
+main = playIO (InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
+              black            -- Background color
+              10               -- Frames per second
+              initialState     -- Initial state
+              view             -- View function
+              input            -- Event function
+              step             -- Step function
 
 data GameState = GameState { playfield :: PlayField
                 , field :: Field
