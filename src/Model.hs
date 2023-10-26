da1875c8c4f0d5207fc8a16b3da0b77ca32a8c79
gijskoppenberg <g.c.koppenberg@students.uu.nl>
2023-10-26 14:29:24 +0200

    heh vorige push was fout

--- src/Model.hs
+++ src/Model.hs
@@ -17,10 +17,11 @@ data GameState = GameState {
     , images :: [Picture]
     , score :: Int
     , ghostTimer :: Int
+    , lastPressed :: Char
 }
 
 initialState :: PacMan -> [Picture] -> Maze -> [Ghost] -> GameState
-initialState pacm p m g = GameState { viewState = Running, elapsedTime = 0 , pacman = initialPacMan, ghosts = g, maze = m , images = p, score = 0,ghostTimer = 0 } 
+initialState pacm p m g = GameState { viewState = Running, elapsedTime = 0 , pacman = initialPacMan, ghosts = g, maze = m , images = p, score = 0,ghostTimer = 0, lastPressed = 'w' } 
 
 initialPacMan :: PacMan
 initialPacMan = PacMan { PacMan.point = (14,23), PacMan.direction = Loadlevel.GoUp }
